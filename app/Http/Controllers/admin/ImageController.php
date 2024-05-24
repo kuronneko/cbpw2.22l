91f2a9c8af5ad6aba9ab2bc2503f80ccdccf9d0b
wdocker <wdocker@wdocker.com>
2024-05-24 00:48:05 -0400

    expand filesize max

--- app/Http/Controllers/admin/ImageController.php
+++ app/Http/Controllers/admin/ImageController.php
@@ -130,7 +130,7 @@ class ImageController extends Controller
     {
 
         $request->validate([
-            'file' => 'required|mimes:mp4,webm,gif,png,jpg,jpeg|max:10240' //10 mb max filesize
+            'file' => 'required|mimes:mp4,webm,gif,png,jpg,jpeg|max:1024000' //1000 mb max filesize
         ]);
 
         $document = $request->file('file');
